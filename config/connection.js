978623f1c4d712839c60f8fa4ef5e8b687f47462
Tito <ejlopez44@gmail.com>
2020-05-24 13:26:42 -0400

    configured connection for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,22 @@
 // Sensitive value protection
 require('dotenv').config()
 
-// MySql Connection..
+// database Connection..
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASSWORD,
-    database: "burgers_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASSWORD,
+        database: "burgers_db"
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
